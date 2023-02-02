d3bf9f3f28d29c8ea687024088b870f39e418574
marco decarlo <marco.decarlo3@gmail.com>
2023-02-02 20:54:45 +0100

    create the buttons section of the post component

--- components/Post.js
+++ components/Post.js
@@ -1,21 +1,44 @@
 import React from "react";
-import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
-
+import {
+    EllipsisHorizontalIcon,
+    HeartIcon,
+    ChatBubbleLeftIcon,
+    BookmarkIcon,
+    FaceSmileIcon,
+} from "@heroicons/react/24/outline";
 export default function Post({ img, userImg, caption, username, id }) {
     return (
         <div className="bg-white my-7 border rounded-md">
             {/* Post Header */}
 
             <div className="flex items-center p-5">
-                <img className="h-12 rounded-full object-cover border p-1 mr-3"  src={userImg} alt={username} />
+                <img
+                    className="h-12 rounded-full object-cover border p-1 mr-3"
+                    src={userImg}
+                    alt={username}
+                />
                 <p className="font-bold flex-1">{username}</p>
-                <EllipsisHorizontalIcon className="h-5"/>
-
+                <EllipsisHorizontalIcon className="h-5" />
             </div>
 
             {/* Post Image */}
 
             <img className="object-cover w-full" src={img} alt="" />
+
+            {/* Post Buttons  */}
+
+                <div className="flex justify-between px-4 pt-4">
+                    <div className="flex space-x-4">
+                        <HeartIcon className="btn" />
+                        <ChatBubbleLeftIcon className="btn" />
+                    </div>
+                    <BookmarkIcon className="btn" />
+                </div>
+
+            {/* Post comments */}
+
+            {/* Post input box */}
+
         </div>
     );
 }
