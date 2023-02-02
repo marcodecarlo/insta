e22c5a635b86fe67c07bd6121db8a6fdb58c1642
marco decarlo <marco.decarlo3@gmail.com>
2023-02-02 21:05:47 +0100

    add the caption and input box of the post component

--- components/Post.js
+++ components/Post.js
@@ -6,7 +6,8 @@ import {
     BookmarkIcon,
     FaceSmileIcon,
 } from "@heroicons/react/24/outline";
-export default function Post({ img, userImg, caption, username, id }) {
+
+export default function Post({img, userImg, caption, username, id}) {
     return (
         <div className="bg-white my-7 border rounded-md">
             {/* Post Header */}
@@ -18,27 +19,41 @@ export default function Post({ img, userImg, caption, username, id }) {
                     alt={username}
                 />
                 <p className="font-bold flex-1">{username}</p>
-                <EllipsisHorizontalIcon className="h-5" />
+                <EllipsisHorizontalIcon className="h-5"/>
             </div>
 
             {/* Post Image */}
 
-            <img className="object-cover w-full" src={img} alt="" />
+            <img className="object-cover w-full" src={img} alt=""/>
 
             {/* Post Buttons  */}
 
-                <div className="flex justify-between px-4 pt-4">
-                    <div className="flex space-x-4">
-                        <HeartIcon className="btn" />
-                        <ChatBubbleLeftIcon className="btn" />
-                    </div>
-                    <BookmarkIcon className="btn" />
+            <div className="flex justify-between px-4 pt-4">
+                <div className="flex space-x-4">
+                    <HeartIcon className="btn"/>
+                    <ChatBubbleLeftIcon className="btn"/>
                 </div>
+                <BookmarkIcon className="btn"/>
+            </div>
 
             {/* Post comments */}
 
+            <p className="p-5 truncate">
+                <span className="font-bold mr-2">{username}</span>
+                {caption}
+            </p>
+
             {/* Post input box */}
 
+            <form className="flex items-center p-4">
+                <FaceSmileIcon className="h-7"/>
+                <input
+                    className="border-none flex-1 focus:ring-0"
+                    type="text"
+                    placeholder="Enter your comment..."
+                />
+                <button className="text-blue-400 font-bold">Post</button>
+            </form>
         </div>
     );
 }
